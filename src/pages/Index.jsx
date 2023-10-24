6530b04aa1b4ff563d5adcfa05048f667d780294
uinuscopr <uinus.copr@uinus.co.kr>
2023-10-24 10:04:15 +0900

    feat(AeLim): 프로젝트 초기 세팅

---
+++ src/pages/Index.jsx
@@ -0,0 +1,19 @@
+import "../styles/global.scss";
+
+export default function Index() {
+  return (
+    <>
+      <div className="container">
+        <h1>프론트엔드 컴포넌트 아카이브</h1>
+        <ul>
+          <li>
+            <p>1. 체크박스</p>
+          </li>
+          <li>
+            <p>2. 모달창</p>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
